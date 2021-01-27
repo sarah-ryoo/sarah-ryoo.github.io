14a92eff923b8840031a14397472ee61774540c6
sarah-ryoo <47673592+sarah-ryoo@users.noreply.github.com>
2021-01-27 11:19:31 -0800

    Experience content change

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -10,9 +10,8 @@ const Experience = () => {
                     <h2 className="School">TRIUMF</h2>
                     <div className="Degree">Frontend Developer</div>
                     <div className="Degree-content">WordPress | UI Design | Docker</div>
-                    <div className="Experience-description">Pioneered a Public Outreach Portal by creating 
-                    an interactive and engaging website to showcase the client's work and increase their
-                    brand name recognition.<br/></div>
+                    <div className="Experience-description">Spearheaded a Public Outreach Portal for TRIUMF by creating 
+                    an interactive and engaging website to showcase their work and increase brand name recognition. <br/></div>
                     <div className="Degree-content">2020</div>
             </div>
 
@@ -20,8 +19,8 @@ const Experience = () => {
                     <h2 className="School">The ReFrame Group</h2>
                     <div className="Degree">Backend Developer</div>
                     <div className="Degree-content">Amazon RDS | JSON | MySQL</div> 
-                    <div className="Experience-description">Implemented a responsive form GUI on the client's existing 
-                    website to automate the process of generating a life insurance quote.</div>
+                    <div className="Experience-description">Developed and implemented a responsive form GUI on the client’s existing 
+                    website to obtain the customer’s information required to generate a life insurance quote.</div>
                     <div className="Degree-content">2020</div> 
             </div>
 
@@ -29,7 +28,9 @@ const Experience = () => {
                     <h2 className="School">We Occupy Earth</h2>
                     <div className="Degree">Backend Developer</div>
                     <div className="Degree-content">App Development | ReactJS | Google API</div> 
-                    <div className="Experience-description">Developed a real time mobile app for natural disaster preparation and awareness with a safety guidance feature.</div>
+                    <div className="Experience-description">A real time mobile application for natural disaster preparation and 
+                    awareness with a safety guidance feature implemented using Google Maps API.<br/><br/>Project was chosen to present at the 
+                    EcoCity World Summit 2019 in Vancouver.</div>
                     <div className="Degree-content"><a href="https://we-occupy-earth.firebaseapp.com/">The App</a>  |  
                     <a href="https://www.youtube.com/watch?v=b3ogAyVLEXQ&ab_channel=KTruong"> Video Walkthrough</a>  |  
                     <a href= { projectposter }> Project Poster</a></div>
