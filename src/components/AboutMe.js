5fb4ead532ee34f64f2ae2acba981c1565d38ff8
sarah-ryoo <47673592+sarah-ryoo@users.noreply.github.com>
2021-01-27 20:01:43 -0800

    Changed About Me content

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -8,9 +8,7 @@ const AboutMe = () => {
             <img src={ profile } alt="Profile"/>
             <p>I'm a chemist turned programmer with a particular interest for data science. 
             Coding was a steep learning curve for me and everday is a new challenge,
-            but I believe those challenges have made me a better programmer today than I was yesterday.<br/>
-            <br/>I'm working towards my goal of using my passion and backgrounds to tackle environmental and 
-            social issues.</p>
+            but I believe those challenges have made me a better programmer today than I was yesterday.</p>
         </div>
     );
 };
